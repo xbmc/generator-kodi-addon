3afd6c93d597a7a8ff5b6150adfacb0a745f1184
Kolja Lampe <razzeee@gmail.com>
2017-01-15 12:23:17 +0100

    Fixed test

--- test/app.js
+++ test/app.js
@@ -6,7 +6,19 @@ var helpers = require('yeoman-test');
 describe('generate contextmenu', function () {
   before(function () {
     return helpers.run(path.join(__dirname, '../generators/app'))
-      .withPrompts({someAnswer: true})
+      .withPrompts({
+        type: 'Contextmenu',
+        scriptid: 'contextmenu.test',
+        scriptname: 'My contextmenu name',
+        kodiVersion: '2.25.0',
+        platforms: 'all',
+        license: 'MIT',
+        authors: 'Me',
+        summary: 'My summary',
+        authorName: 'My real name',
+        email: 'test@test.de',
+        website: 'www.kodi.tv'
+      })
       .toPromise();
   });
 
