4817dcdf63d1d91d0c4fff78cd779678c9d34db7
Kolja Lampe <razzeee@gmail.com>
2017-01-18 19:10:35 +0100

    Improved script boilerplate

--- generators/app/templates/script.py
+++ generators/app/templates/script.py
@@ -1,6 +1,10 @@
+# -*- coding: utf-8 -*-
+
 import xbmcaddon
 import xbmcgui
 
+# Put your code here, this is just an example showing
+# a textbox as soon as this addon gets called
 addon = xbmcaddon.Addon()
 addonname = addon.getAddonInfo('name')
 
