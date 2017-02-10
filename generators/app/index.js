b566f13d15c7288d9ee40965fc60384975b5af0a
Kolja Lampe <razzeee@gmail.com>
2017-02-10 13:02:33 +0100

    We should have no file named .gitignore as git and maybe even npm thinks it's a real gitignore. We just want to use it as a template.

--- generators/app/index.js
+++ generators/app/index.js
@@ -156,7 +156,7 @@ module.exports = yeoman.extend({
       }
     );
     this.fs.copy(
-      this.templatePath('.gitignore'),
+      this.templatePath('gitignore'),
       this.destinationPath('.gitignore')
     );
     this.fs.copy(
